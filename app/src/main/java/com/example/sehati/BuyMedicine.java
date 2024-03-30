d9fae304d6a8dbc9b50dc0f6de5414f2903670c7
M Yasir Rizqi <rizqiyasir80@gmail.com>
2024-03-30 19:14:39 +0700

    medicine

--- app/src/main/java/com/example/sehati/BuyMedicine.java
+++ app/src/main/java/com/example/sehati/BuyMedicine.java
@@ -2,13 +2,30 @@ package com.example.sehati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BuyMedicine extends AppCompatActivity {
-
+    Button buy_medicine;
+    TextView cost_medicine;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_medicine);
+        buy_medicine = findViewById(R.id.bt_buy_medicine);
+        cost_medicine = findViewById(R.id.tv_amountMedicine);
+
+        buy_medicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent  intent = new Intent(BuyMedicine.this,ConfirmMedicine.class);
+                intent.putExtra("harga_total",cost_medicine.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
