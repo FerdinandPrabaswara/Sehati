3d1e431d3013c3f5e7bb3bc974ac66c8453f154c
ferdinandp@student.ub.ac.id <ferdinandp@Student.ub.ac.id>
2024-03-30 21:43:23 +0700

    adding intent

--- app/src/main/java/com/example/sehati/Login.java
+++ app/src/main/java/com/example/sehati/Login.java
@@ -2,13 +2,50 @@ package com.example.sehati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    Button btnLogin;
+    EditText etEmail, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
+
+        btnLogin    = findViewById(R.id.btn_login);
+        etEmail     = findViewById(R.id.et_email);
+        etPassword  = findViewById(R.id.et_password);
+
+//        for now use:
+//        email   : sehati@student.ub.ac.id
+//        pass    : rahasiakita
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email    = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                Bundle name     = getIntent().getExtras();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(Login.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else if(email.equals("sehati@student.ub.ac.id") && password.equals("rahasiakita")){
+                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent_login = new Intent(Login.this, Home.class);
+                    intent_login.putExtra("name", name);
+                    startActivity(intent_login);
+                } else {
+                    Toast.makeText(Login.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sehati/Register.java
+++ app/src/main/java/com/example/sehati/Register.java
@@ -2,13 +2,47 @@ package com.example.sehati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    Button btnRegist;
+    EditText etName, etEmail, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+
+        etName      = findViewById(R.id.et_name);
+        etEmail     = findViewById(R.id.et_email);
+        etPassword  = findViewById(R.id.et_password);
+        btnRegist   = findViewById(R.id.btn_regist);
+
+        btnRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name     = etName.getText().toString();
+                String email    = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Register.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                    Intent intent_regist = new Intent(Register.this, Login.class);
+                    intent_regist.putExtra("name", etName.getText().toString());
+                    startActivity(intent_regist);
+                }
+
+            }
+        });
+
     }
 }
