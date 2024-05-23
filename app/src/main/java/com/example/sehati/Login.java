1c13261e03d050389621f3a78a13e3b0e56b65a1
ferdinandp@student.ub.ac.id <ferdinandp@Student.ub.ac.id>
2024-05-23 07:03:29 +0700

    modify login and register layout

--- app/src/main/java/com/example/sehati/Login.java
+++ app/src/main/java/com/example/sehati/Login.java
@@ -1,5 +1,6 @@
 package com.example.sehati;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,43 +8,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+    FirebaseAuth mAuth;
     Button btnLogin;
     EditText etEmail, etPassword;
+    TextView tvRegist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         btnLogin    = findViewById(R.id.btn_login);
         etEmail     = findViewById(R.id.et_email);
         etPassword  = findViewById(R.id.et_password);
+        tvRegist    = findViewById(R.id.regist);
 
-//        for now use:
-//        email   : sehati@student.ub.ac.id
-//        pass    : rahasiakita
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email    = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
-                Bundle name     = getIntent().getExtras();
 
                 if (email.isEmpty() || password.isEmpty()){
                     Toast.makeText(Login.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
-                } else if(email.equals("sehati@student.ub.ac.id") && password.equals("rahasiakita")){
-                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    Intent intent_login = new Intent(Login.this, Home.class);
-                    intent_login.putExtra("name", name);
-                    startActivity(intent_login);
                 } else {
-                    Toast.makeText(Login.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent_login = new Intent(Login.this, Home.class);
+                                        startActivity(intent_login);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
 
+            }
+        });
+
+        tvRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(Login.this, Register.class);
+                startActivity(intentRegister);
             }
         });
     }
--- app/src/main/java/com/example/sehati/Register.java
+++ app/src/main/java/com/example/sehati/Register.java
@@ -1,5 +1,6 @@
 package com.example.sehati;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
@@ -35,14 +43,34 @@ public class Register extends AppCompatActivity {
                 if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(Register.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                    Intent intent_regist = new Intent(Register.this, Login.class);
-                    intent_regist.putExtra("name", etName.getText().toString());
-                    startActivity(intent_regist);
+                    // Create user in Firebase Authentication
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Menyimpan nama ke realtime database
+                                        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                                        // Menyimpan nama ke realtime database di dalam tabel user dengan user ID
+                                        DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+                                        currentUserDb.child("name").setValue(name);
+
+                                        Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent_regist = new Intent(Register.this, Login.class);
+                                        intent_regist.putExtra("name", name);
+                                        startActivity(intent_regist);
+                                    } else {
+                                        Toast.makeText(Register.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
 
             }
         });
 
     }
+
+
 }
