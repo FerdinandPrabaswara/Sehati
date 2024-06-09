214d1e4c8ae7c9026ea14ccffa7e2e6b6287fb66
ferdinandp@student.ub.ac.id <ferdinandp@Student.ub.ac.id>
2024-06-09 14:31:32 +0700

    add icon and get name to Homepage

--- app/src/main/java/com/example/sehati/Login.java
+++ app/src/main/java/com/example/sehati/Login.java
@@ -1,13 +1,18 @@
 package com.example.sehati;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,30 +20,36 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
-    FirebaseAuth mAuth;
+    private FirebaseAuth auth;
     Button btnLogin;
     EditText etEmail, etPassword;
     TextView tvRegist;
+    ImageView ivShowHidePassword;
+
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         btnLogin    = findViewById(R.id.btn_login);
         etEmail     = findViewById(R.id.et_email);
         etPassword  = findViewById(R.id.et_password);
         tvRegist    = findViewById(R.id.regist);
+        ivShowHidePassword   = findViewById(R.id.iv_show_hide_password);
 
         String emailFromRegister = getIntent().getStringExtra("email");
-        if (emailFromRegister != null && !emailFromRegister.isEmpty()) {
-            etEmail.setText(emailFromRegister);
-        }
-
+        etEmail.setText(emailFromRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,26 +57,32 @@ public class Login extends AppCompatActivity {
                 String email    = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if (email.isEmpty() || password.isEmpty()){
+                if (email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(Login.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 } else {
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                        Intent intent_login = new Intent(Login.this, Home.class);
-                                        startActivity(intent_login);
-                                        finish();
-                                    } else {
-                                        Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
+                    loginUser(email, password);
                 }
-
             }
         });
+
+        ivShowHidePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    // Hide password
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    ivShowHidePassword.setImageResource(R.drawable.hide_password);
+                } else {
+                    // Show password
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                    ivShowHidePassword.setImageResource(R.drawable.show_password);
+                }
+                // Move cursor to the end of the text
+                etPassword.setSelection(etPassword.length());
+                isPasswordVisible = !isPasswordVisible;
+            }
+        });
+
         tvRegist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,5 +90,37 @@ public class Login extends AppCompatActivity {
                 startActivity(intentRegister);
             }
         });
+    }
+    private void loginUser(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user != null) {
+                        database.getReference("User").child(user.getUid()).get().addOnSuccessListener(dataSnapshot -> {
+                            if (dataSnapshot.exists()) {
+                                // user is a user
+                                Intent intent = new Intent(Login.this, Home.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).addOnFailureListener(e -> {
+                            Toast.makeText(Login.this, "Failed to get data from database", Toast.LENGTH_SHORT).show();
+                        });
+                    }
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        Toast.makeText(Login.this, "Invalid email or password. Please try again.", Toast.LENGTH_SHORT).show();} catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(Login.this, "Login failed. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
