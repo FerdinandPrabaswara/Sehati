d8564dc1a30439186b1bf5b76f0d6a694f59f6f1
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2024-06-09 15:09:37 +0700

    [add] recycler view and intent in meditation page

---
+++ app/src/main/java/com/example/sehati/model/User.java
@@ -0,0 +1,17 @@
+package com.example.sehati.model;
+
+public class User {
+    public String name, email, password;
+
+    public User(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+}
+
